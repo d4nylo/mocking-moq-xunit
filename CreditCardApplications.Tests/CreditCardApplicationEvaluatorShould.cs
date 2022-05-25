21f71d86db1b1358ddcb817ff9a21b5bd8880bfe
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-05-25 14:21:53 +0100

    Checking a Mock Method Was Called Multiple Times with Different Values

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Moq;
 using Xunit;
 
@@ -400,5 +401,32 @@ public class CreditCardApplicationEvaluatorShould
 
         var secondDecision = sut.Evaluate(application);
         Assert.Equal(CreditCardApplicationDecision.AutoDeclined, secondDecision);
+    }
+
+    [Fact]
+    public void ReferInvalidFrequentFlyerApplications_MultipleCallsSequence()
+    {
+        var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+        mockValidator
+            .Setup(x => x.ServiceInformation.License.LicenseKey)
+            .Returns("OK");
+
+        var frequentFlyerNumbersPassed = new List<string>();
+
+        mockValidator.Setup(x => x.IsValid(Capture.In(frequentFlyerNumbersPassed)));
+
+        var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+        var application1 = new CreditCardApplication {Age = 25, FrequentFlyerNumber = "aa"};
+        var application2 = new CreditCardApplication {Age = 25, FrequentFlyerNumber = "bb"};
+        var application3 = new CreditCardApplication {Age = 25, FrequentFlyerNumber = "cc"};
+
+        sut.Evaluate(application1);
+        sut.Evaluate(application2);
+        sut.Evaluate(application3);
+
+        // Assert that IsValid was called 3 times with "aa", "bb", and "cc"
+        Assert.Equal(new List<string> {"aa", "bb", "cc"}, frequentFlyerNumbersPassed);
     }
 }
