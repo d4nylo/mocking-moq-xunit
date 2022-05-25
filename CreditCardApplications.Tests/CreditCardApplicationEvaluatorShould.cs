680ea2b362cf2f032e663c9cc3f9d8e94f2647e9
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-05-25 14:46:13 +0100

    Improving Mock Setup Readability with LINQ to Mocks

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -455,4 +455,32 @@ public class CreditCardApplicationEvaluatorShould
 
         Assert.Equal(CreditCardApplicationDecision.ReferredToHumanFraudRisk, decision);
     }
+
+    [Fact]
+    public void LinqToMocks()
+    {
+        // var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+        //
+        // mockValidator
+        //     .Setup(x => x.ServiceInformation.License.LicenseKey)
+        //     .Returns("OK");
+        //
+        // mockValidator
+        //     .Setup(x => x.IsValid(It.IsAny<string>()))
+        //     .Returns(true);
+
+        var mockValidator = Mock.Of<IFrequentFlyerNumberValidator>(
+            validator =>
+                validator.ServiceInformation.License.LicenseKey == "OK" &&
+                validator.IsValid(It.IsAny<string>()) == true
+        );
+
+        var sut = new CreditCardApplicationEvaluator(mockValidator);
+
+        var application = new CreditCardApplication {Age = 25};
+
+        var decision = sut.Evaluate(application);
+
+        Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+    }
 }
